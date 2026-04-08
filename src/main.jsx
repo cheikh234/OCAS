592d3d0c2ed567086c5c6b902840de9bf4789a7b
cheikh <cheikhbass2001@gmail.com>
2026-04-08 19:59:48 +1200

    fix: ajout de BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'  // ← AJOUTE CETTE LIGNE
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>  {/* ← AJOUTE CE WRAPPER */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
